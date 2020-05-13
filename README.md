23113dedcc164884b7c795b28e4a8a2378b5be0e
Oscar Baracos <oscar.baracos@gmail.com>
2020-05-13 00:17:09 -0400

    Split into general and programming zens

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ Sending some \~zen\~ your way
 
 Built with Express
 
+Check it out for yourself, from your browser or with cURL: 
+* General zen: https://oscar-zen.herokuapp.com/
+* Programming zen: https://oscar-zen.herokuapp.com/programming
+
 To run:
 ```sh
 npm run start
 ```
 
-Check it out for yourself: https://oscar-zen.herokuapp.com/
--- app.js
+++ app.js
@@ -4,16 +4,28 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const zens = [
-    "Be lazy",
     "Best is the enemy of good",
     "A problem well-stated is half solved",
     "He who angers you conquers you",
     "Accept that the future will play out differently than you think it will"
 ]
 
-app.get('/', (req, res) => {
+const programming_zens = [
+    "Be lazy",
+    "Best is the enemy of good",
+    "A problem well-stated is half solved",
+    "Read the manual, or the docs",
+    "It compiles or it runs != it's correct"
+]
+
+app.get(['/', '/general'], (req, res) => {
     let i = Math.floor(Math.random() * zens.length);
     res.send(zens[i]);
+});
+
+app.get('/programming', (req, res) => {
+    let i = Math.floor(Math.random() * programming_zens.length);
+    res.send(programming_zens[i]);
 });
 
 app.listen(port, () => console.log(`Zen running on port ${port}`));
